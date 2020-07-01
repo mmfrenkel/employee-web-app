67c2cef60aa81e1dda84551d583aa4e48d4347e7
mmfrenkel <megan.frenkel@gmail.com>
2020-07-01 16:28:24 -0400

    adding jpa support

---
+++ src/main/java/com/learning/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.learning.springboot.thymeleafdemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.learning.springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	
+	// that's it... no need to write any code!
+}
---
+++ src/main/java/com/learning/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,54 @@
+package com.learning.springboot.thymeleafdemo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.learning.springboot.thymeleafdemo.dao.EmployeeRepository;
+import com.learning.springboot.thymeleafdemo.entity.Employee;
+
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+	
+	
+	private EmployeeRepository employeeRepository;
+	
+	@Autowired                // qualify which bean to use via bean id
+	public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+		this.employeeRepository = employeeRepository;
+	}
+
+	@Override 
+	public List<Employee> findAll() {
+		return employeeRepository.findAll();
+	}
+
+	@Override 
+	public Employee findById(int id) {
+		Optional<Employee> result = employeeRepository.findById(id);
+		
+		Employee employee = null;
+		
+		if (result.isPresent()) {
+			employee = result.get();
+		} else {
+			throw new RuntimeException("Did not find Employee with id: " + id);
+		}
+		
+		return employee;
+	}
+
+	@Override 
+	public void save(Employee employee) {
+		employeeRepository.save(employee);
+	}
+
+	@Override 
+	public void delete(int id) {
+		employeeRepository.deleteById(id);
+	}
+
+}
