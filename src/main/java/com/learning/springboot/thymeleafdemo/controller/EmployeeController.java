f8b5654455d6a01ae343127210ed8c08084b7fba
mmfrenkel <megan.frenkel@gmail.com>
2020-07-01 17:23:23 -0400

    enabling update and delete with app

--- src/main/java/com/learning/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/learning/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,41 +1,77 @@
 package com.learning.springboot.thymeleafdemo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.learning.springboot.thymeleafdemo.entity.Employee;
+import com.learning.springboot.thymeleafdemo.service.EmployeeService;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 	
-	private List<Employee> employees;
+	private EmployeeService employeeService;
 	
-	@PostConstruct
-	private void loadData() {
-		// create employees, just in memory for now
-		
-		Employee employee1 = new Employee(1, "Leslie", "Andrews", "landrew@gmail.com");
-		Employee employee2 = new Employee(2, "Harper", "Frenkel", "harper@dogs.com");
-		Employee employee3 = new Employee(3, "Andy", "Grandy", "andy@grants.com");
-		
-		employees = new ArrayList<>();
-		employees.add(employee1);
-		employees.add(employee2);
-		employees.add(employee3);
+	@Autowired
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
 	}
 	
 	@GetMapping("/list")
 	public String listEmployees(Model model) {
+		
+		List<Employee> employees = employeeService.findAll();
+		
 		model.addAttribute("employees", employees);
-		return "list-employees";
+		
+		return "employees/list-employees";
+	}
+	
+	@GetMapping("/showEmployeeFormCreate")
+	public String showEmployeeFormCreate(Model model) {
+		
+		// create model attribute to hold/bind form data
+		Employee employee = new Employee();
+		
+		model.addAttribute("employee", employee);
+		
+		return "employees/employee-form";
+	}
+	
+	@PostMapping("/showFormForUpdate")
+	public String showEmployeeFormUpdate(@RequestParam("employeeId") int id, Model model) {
+
+		// find employee in database
+		Employee employee = employeeService.findById(id);
+		
+		// add employee to model in order to pre-populate the form
+		model.addAttribute("employee", employee);
+		
+		return "employees/employee-form";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		
+		employeeService.save(employee);
+		
+		return "redirect:/employees/list";
+	}
+
+	@GetMapping("/delete")
+	public String delete(@RequestParam("employeeId") int id) {
+		
+		employeeService.delete(id);
+		
+		return "redirect:/employees/list";
 	}
 
 }
--- src/main/java/com/learning/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ src/main/java/com/learning/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -1,10 +1,13 @@
 package com.learning.springboot.thymeleafdemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.learning.springboot.thymeleafdemo.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	
-	// that's it... no need to write any code!
+	
+	public List<Employee> findAllByOrderByLastNameAsc();
 }
